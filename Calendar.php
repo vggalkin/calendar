102540d806c211feb6f2927aca1cb829809e01db
VLADIMIR G <v.g.galkin@gmail.com>
2021-09-13 20:06:52 +0300

    Update view holiday add birthdate

--- Calendar.php
+++ Calendar.php
@@ -137,7 +137,7 @@ class Calendar extends Widget
                     $calendar_view .= '<div class="todate" style="background-color: #f18d8d;">' . $d . '</div>';
                 } else if ($today == $now) {
                     $calendar_view .= '<div class="todate" style="background-color: #7bea7b;">' . $d . '</div>';
-                } else if ($m == $birth_date_exp[1] and $d == $birth_date_exp[2]) {
+                } else if (strftime('%m',$today) == $birth_date_exp[1] and strftime('%d',$today) == $birth_date_exp[2]) {
                     $calendar_view .= '<div class="todate" style="background-color: #ee5e13;">' . $d . '</div>';
                 } else {
                     $calendar_view .= $d;
